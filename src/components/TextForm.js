67a007db249b7a1177d92a0a527618abd749c78b
kabeer099 <devilhenry670@gmail.com>
2023-04-10 10:53:40 +0500

    added more logic to textutils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,11 +1,16 @@
 import React , { useState } from "react";
 
 export default function TextForm(props) {
-    const [text, setText] = useState('Enter Text Here');
+
+    const [text, setText] = useState('');
     const handleUpClick =()=> {
         console.log('Uppercase was clicked' + text);
         const textUpperCase = text.toUpperCase();
         setText(textUpperCase);
+    }
+    const handlelowClick =()=> {
+      const textLowercase = text.toLowerCase();
+      setText(textLowercase);
     }
     const handleOnChange =(event)=> {
         console.log('On Change');
@@ -17,7 +22,15 @@ export default function TextForm(props) {
   <h1>{props.heading}</h1>
 <div className="mb-3">
   <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
-  <button className="btn btn-primary mt-2" onClick={handleUpClick}>Convert to Uppercase</button>
+  <button className="btn btn-primary my-2 mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+  <button className="btn btn-primary my-2 mx-2" onClick={handlelowClick}>Convert to Lowercase</button>
+</div>
+<div className="container">
+    <h2>Your Text Summary</h2>
+    <p>{text.split(' ').length} words and {text.length} characters</p>
+    <p>{0.008 * text.split(' ').length } Minutes to reads </p>
+    <h3>Preview</h3>
+    <p>{text}</p>
 </div>
     </div>
   )
