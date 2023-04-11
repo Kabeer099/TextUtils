72a14e0e5e4a57d3b0227d6c41d49e5945b3f4e5
kabeer099 <devilhenry670@gmail.com>
2023-04-11 12:59:49 +0500

    Created About page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       <Navbar title="TextUtils" aboutText="About us" />
       <div className="container my-3">
 
-      <TextForm heading="Enter the text to Analyze below"/>
+      {/* <TextForm heading="Enter the text to Analyze below"/> */}
+      <About />
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,16 +4,39 @@ export default function TextForm(props) {
 
     const [text, setText] = useState('');
     const handleUpClick =()=> {
-        console.log('Uppercase was clicked' + text);
+        // console.log('Uppercase was clicked' + text);
         const textUpperCase = text.toUpperCase();
         setText(textUpperCase);
     }
-    const handlelowClick =()=> {
+    const handleLowClick =()=> {
       const textLowercase = text.toLowerCase();
       setText(textLowercase);
     }
+    const handleClearClick =()=>{
+      const clearText = '';
+        setText(clearText);
+    }
+    const handleClipBoardClick =()=>{
+      navigator.clipboard.writeText(text);
+    }
+    const handleSpeakClick = () => {
+      let msg = new SpeechSynthesisUtterance();
+      msg.text = text;
+      window.speechSynthesis.speak(msg);
+    }
+    const handleReverseTextClick =()=> {
+      const reverseText = text.split("").reverse();
+      // reverseText = reverseText.reverse();
+      let newText = reverseText.join("");
+      setText(newText);
+    }
+    const handleTiltTextClick =()=> {
+     let tiltText = text.toUpperCase() + text.substring(1).toLowerCase();
+     setText(tiltText);
+    }
+
     const handleOnChange =(event)=> {
-        console.log('On Change');
+        // console.log('On Change');
         setText(event.target.value);
     }
 
@@ -22,13 +45,18 @@ export default function TextForm(props) {
   <h1>{props.heading}</h1>
 <div className="mb-3">
   <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
-  <button className="btn btn-primary my-2 mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-  <button className="btn btn-primary my-2 mx-2" onClick={handlelowClick}>Convert to Lowercase</button>
+  <button className="btn btn-success my-2 mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+  <button className="btn btn-success my-2 mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
+  <button className="btn btn-success my-2 mx-2" onClick={handleClearClick}>Clear Text</button>
+  <button className="btn btn-success my-2 mx-2" onClick={handleSpeakClick}>Speech</button>
+  <button className="btn btn-success my-2 mx-2" onClick={handleClipBoardClick}>Copy Text to Click board</button>
+  <button className="btn btn-success my-2 mx-2" onClick={handleReverseTextClick}>Reverse Text</button>
+  <button className="btn btn-success my-2 mx-2" onClick={handleTiltTextClick}>Tilt Text</button>
 </div>
 <div className="container">
     <h2>Your Text Summary</h2>
-    <p>{text.split(' ').length} words and {text.length} characters</p>
-    <p>{0.008 * text.split(' ').length } Minutes to reads </p>
+    <p>{text === '' ? '0' :text.split(' ').length} words and {text.length} characters</p>
+    <p>{text === '' ? '0' :0.008 * text.split(' ').length } Minutes to reads </p>
     <h3>Preview</h3>
     <p>{text}</p>
 </div>
